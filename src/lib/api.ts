b847fbd80d9e81a01f2e351be9466211d34b95d6
nnndaru <aji.prabandaru@gmail.com>
2026-01-12 01:23:56 +0700

    setup env and axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use((config) => {
